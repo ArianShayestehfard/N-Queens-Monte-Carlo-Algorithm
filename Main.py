5999641256dfc764220432798108304a9138d06d
Arian Shayestehfard <arian.shayestehfard@gmail.com>
2026-07-09 12:13:54 +0330

    Main Update!

--- Main.py
+++ Main.py
@@ -5,3 +5,13 @@ def safe(board, row, col):
         if board[i] == col or abs(board[i]-col) == row-i:
             return False
     return True
+
+def exact_nodes(board, row, n):
+    if row == n:
+        return 1
+    total = 0
+    for col in range(n):
+        if safe(board, row, col):
+            board[row] = col
+            total += exact_nodes(board, row+1, n)
+    return total
