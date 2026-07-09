9b71595063f2a825ffea84c8d3b3bc25a5ef94b0
Arian Shayestehfard <arian.shayestehfard@gmail.com>
2026-07-09 12:14:16 +0330

    Main Update!

--- Main.py
+++ Main.py
@@ -15,3 +15,21 @@ def exact_nodes(board, row, n):
             board[row] = col
             total += exact_nodes(board, row+1, n)
     return total
+
+def monte_carlo_nodes(n, trials):
+    total = 0
+    for _ in range(trials):
+        board = [-1]*n
+        row = 0
+        nodes = 0
+        while True:
+            nodes += 1
+            if row == n:
+                total += nodes
+                break
+            cols = [c for c in range(n) if safe(board, row, c)]
+            if not cols:
+                break
+            board[row] = random.choice(cols)
+            row += 1
+    return total/trials
